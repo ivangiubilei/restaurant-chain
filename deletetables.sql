c6d544e9d57bcf355e03266ca20af0a663dc2f1d
EdoardoMauriello <33875515+EdoardoMauriello@users.noreply.github.com>
2024-04-03 16:44:19 +0200

    Create deletetables.sql

---
+++ deletetables.sql
@@ -0,0 +1,9 @@
+drop table offers;
+drop table account;
+drop table client;
+drop table shipment;
+drop table supplier;
+drop table employed;
+drop table employee;
+drop table restaurant;
+drop table chain;
